8348a0a4e11ff1fd07865e88238d277af75c1bb3
Milan Santosi <milan.santosi@gmail.com>
2025-02-18 12:12:10 +0100

    Auto-save: 2025-02-18 12:12:10 - Cycle 14

--- async-data-fetcher/src/main/java/com/example/asyncdatafetcher/service/DataFetchService.java
+++ async-data-fetcher/src/main/java/com/example/asyncdatafetcher/service/DataFetchService.java
@@ -10,6 +10,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
 
 @Service
 public class DataFetchService {
