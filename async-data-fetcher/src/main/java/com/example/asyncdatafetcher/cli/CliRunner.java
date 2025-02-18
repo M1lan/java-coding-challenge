9cdafc0f499af6b1cbcbb84bf7ef9a63cc60498c
Milan Santosi <milan.santosi@gmail.com>
2025-02-18 12:03:21 +0100

    learn about CLI runner in SBoot, add one too..

---
+++ async-data-fetcher/src/main/java/com/example/asyncdatafetcher/cli/CliRunner.java
@@ -0,0 +1,23 @@
+package com.example.asyncdatafetcher.cli;
+
+import com.example.asyncdatafetcher.service.DataFetchService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+
+public class CliRunner implements CommandLineRunner {
+  private final DataFetchService s;
+  private final ObjectMapper om;
+
+  public CliRunner(DataFetchService s, ObjectMapper om) {
+    this.s = s;
+    this.om = om;
+  }
+
+  public void run(String... args) throws Exception {
+    if (args.length > 0 && "cli".equalsIgnoreCase(args[0])) {
+      var m = s.fetchMergedData().block();
+      System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(m));
+      System.exit(0);
+    }
+  }
+}
