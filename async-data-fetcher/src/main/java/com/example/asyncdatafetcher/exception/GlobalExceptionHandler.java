fffc7099e8603ecdba0e46b24dc173184539a7fa
Milan Santosi <milan.santosi@gmail.com>
2025-02-18 14:44:38 +0100

    actually add GlobalExceptionHandler file :-)

---
+++ async-data-fetcher/src/main/java/com/example/asyncdatafetcher/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.asyncdatafetcher.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
+
+/**
+ * Global exception handler to catch and handle exceptions across the application.
+ * <p>
+ * This class uses {@link RestControllerAdvice} to globally intercept exceptions thrown by REST controllers.
+ * When an exception occurs, it returns a {@link Mono} wrapping a {@link ResponseEntity} containing an error message.
+ * </p>
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	/**
+	 * Handles any {@link Exception} thrown in the application.
+	 * <p>
+	 * This method captures all exceptions and returns a {@link Mono} containing a {@link ResponseEntity}
+	 * with a status of {@link HttpStatus#INTERNAL_SERVER_ERROR} and a simple error message.
+	 * </p>
+	 *
+	 * @param ex the exception that was thrown
+	 * @return a {@link Mono} that emits a {@link ResponseEntity} with an error message
+	 */
+	@ExceptionHandler(Exception.class)
+	public Mono<ResponseEntity<String>> handleException(Exception ex) {
+		return Mono.just(
+						 ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+						 .body("Internal Server Error: " + ex.getMessage()));
+	}
+}
