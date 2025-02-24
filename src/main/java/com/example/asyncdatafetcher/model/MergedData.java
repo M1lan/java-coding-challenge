ac348496dba86dbaa75e98bbd289cbb78edd3e0b
Milan Santosi <milan.santosi@gmail.com>
2025-02-24 18:04:52 +0100

    add posts and user data models

---
+++ src/main/java/com/example/asyncdatafetcher/model/MergedData.java
@@ -0,0 +1,21 @@
+package com.example.asyncdatafetcher.model;
+
+import java.util.List;
+
+public class MergedData {
+    private User user;
+    private List<Post> posts;
+
+    public MergedData() {}
+
+    public MergedData(User user, List<Post> posts) {
+        this.user = user;
+        this.posts = posts;
+    }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public List<Post> getPosts() { return posts; }
+    public void setPosts(List<Post> posts) { this.posts = posts; }
+}
