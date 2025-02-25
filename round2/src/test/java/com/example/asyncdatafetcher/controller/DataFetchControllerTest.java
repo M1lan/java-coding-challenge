48cb1c174acedb17966c02fa80e2ec112285d614
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 05:31:23 +0100

    new DataFetchControllerTest; also add justfile

--- round2/src/test/java/com/example/asyncdatafetcher/controller/DataFetchControllerTest.java
+++ round2/src/test/java/com/example/asyncdatafetcher/controller/DataFetchControllerTest.java
@@ -1,26 +1,12 @@
 package com.example.asyncdatafetcher.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class DataFetchControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void testGetMessage() throws Exception {
-        mockMvc.perform(get("/"))
-			.andExpect(status().isOk())
-			.andExpect(content().string("hello world"));
+@RestController
+public class TestExceptionController {
+    @GetMapping("/error")
+    public String throwException() {
+        throw new RuntimeException("forced error");
     }
 }
