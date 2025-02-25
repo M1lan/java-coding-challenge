3b62ff6a794bf354c3933e4a7a39d1f811b89770
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 06:31:56 +0100

    scan sub-pkgs for bean defs

--- round2/src/main/java/com/example/asyncdatafetcher/AsyncDataFetcherApplication.java
+++ round2/src/main/java/com/example/asyncdatafetcher/AsyncDataFetcherApplication.java
@@ -2,8 +2,10 @@ package com.example.asyncdatafetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.asyncdatafetcher")
 public class AsyncDataFetcherApplication {
     public static void main(String[] args) {
         SpringApplication.run(AsyncDataFetcherApplication.class, args);
