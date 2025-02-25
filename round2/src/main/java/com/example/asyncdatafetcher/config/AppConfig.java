088bc528b365780335c58da2e1e32d74bb56e1d4
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 06:24:25 +0100

    fix NoSuchBeanDefinitionException

--- round2/src/main/java/com/example/asyncdatafetcher/config/AppConfig.java
+++ round2/src/main/java/com/example/asyncdatafetcher/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.asyncdatafetcher.config;
 
+import com.example.asyncdatafetcher.service.ApiClient;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +13,10 @@ public class AppConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
+    }
+
+    @Bean
+    public ApiClient apiClient() {
+        return new ApiClient();
     }
 }
