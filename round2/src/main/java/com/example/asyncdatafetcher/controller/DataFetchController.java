f4984653eb161f27bee5a6f0005ab46e3753c03e
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 05:08:49 +0100

    implement DataFetchController that uses DataFetchService

--- round2/src/main/java/com/example/asyncdatafetcher/controller/DataFetchController.java
+++ round2/src/main/java/com/example/asyncdatafetcher/controller/DataFetchController.java
@@ -1,12 +1,20 @@
 package com.example.asyncdatafetcher.controller;
 
+import com.example.asyncdatafetcher.model.MergedData;
+import com.example.asyncdatafetcher.service.DataFetchService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataFetchController {
+    private final DataFetchService dataFetchService;
+
+    public DataFetchController(DataFetchService dataFetchService) {
+        this.dataFetchService = dataFetchService;
+    }
+
     @GetMapping("/")
-    public String getMessage() {
-        return "hello world";
+    public MergedData getMergedData() {
+        return dataFetchService.fetchMergedData();
     }
 }
