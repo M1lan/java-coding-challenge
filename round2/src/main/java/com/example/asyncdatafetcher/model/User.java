c61e9e83f610460806c74f33eb0fab0cd48ff6d2
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 05:18:25 +0100

    updated user data model

--- round2/src/main/java/com/example/asyncdatafetcher/model/User.java
+++ round2/src/main/java/com/example/asyncdatafetcher/model/User.java
@@ -8,15 +8,35 @@ public class User {
 
     public User() {}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getName() {
+        return name;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
