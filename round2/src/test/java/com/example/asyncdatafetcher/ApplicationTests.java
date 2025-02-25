272c86e4aa01458ebcc55532af992971cf66b3b6
Milan Santosi <milan.santosi@gmail.com>
2025-02-25 06:49:29 +0100

    none webenv

--- round2/src/test/java/com/example/asyncdatafetcher/ApplicationTests.java
+++ round2/src/test/java/com/example/asyncdatafetcher/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.asyncdatafetcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AsyncDataFetcherApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ApplicationTests {
     @Test
     void contextLoads() {
